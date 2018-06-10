147d4f5a5a7641553ee93cf4e75311c05703307e
Vini Salazar <viniws@gmail.com>
2018-06-10 14:10:41 -0300

    created data/csv/all.csv to remove clutter

--- analysis/cartopy/surface_chl.py
+++ analysis/cartopy/surface_chl.py
@@ -6,9 +6,7 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from cartopy.io.shapereader import Reader
-import csv
 import geopandas as gp
-import pandas as pd
 import numpy as np
 
 def main(arg, arg2=None):
@@ -33,12 +31,12 @@ def main(arg, arg2=None):
 		ax.gridlines(draw_labels=True)
 
 		# Os pontos de coleta e os dados vêm de arquivos csv
-		lats = arg[key][0].values
-		lons = arg[key][1].values
-		data = np.genfromtxt(arg[key][2], delimiter=',')
+		lats = np.genfromtxt(arg[key][0], delimiter=',')
+		lons = np.genfromtxt(arg[key][1], delimiter=',')
+		data = np.genfromtxt(arg[key][2], delimiter=';')
 		# Assim como a data que vai aparecer no plot
-		s = arg[key][2]
-		d, m, y = s[9:11], s[11:13], '2017'
+		s = arg[key][0]
+		d, m, y = s[9:11], s[11:13], '2018'
 		dmy = '-'.join([d, m, y])
 
 		# Imagem de background
@@ -67,14 +65,13 @@ def main(arg, arg2=None):
 		else:
 			plt.show()
 
-df = pd.read_csv('data/csv/all.csv', delimiter = ';')
 
 all = {
 
-'saida1' : (df.loc[df['Data'] == '25-Jan-17']['Lat'], df.loc[df['Data'] == '25-Jan-17']['Lon'], 'data/csv/2501_temp.csv'),
-'saida2' : (df.loc[df['Data'] == '27-May-17']['Lat'], df.loc[df['Data'] == '27-May-17']['Lon'], 'data/csv/2705_temp.csv'),
-'saida3' : (df.loc[df['Data'] == '08-Jul-17']['Lat'], df.loc[df['Data'] == '08-Jul-17']['Lon'], 'data/csv/0807_temp.csv'),
-'saida4' : (df.loc[df['Data'] == '1-Oct-17']['Lat'], df.loc[df['Data'] == '1-Oct-17']['Lon'], 'data/csv/0110_temp.csv')
+'saida1' : ('data/csv/2501_lat.csv', 'data/csv/2501_lon.csv', 'data/cc_nutrients/saida1cc.csv')#,
+#'saida2' : ('data/csv/2705_lat.csv', 'data/csv/2705_lon.csv', 'data/csv/2705_temp.csv'),
+#'saida3' : ('data/csv/0807_lat.csv', 'data/csv/0807_lon.csv', 'data/csv/0807_temp.csv'),
+#'saida4' : ('data/csv/0110_lat.csv', 'data/csv/0110_lon.csv', 'data/csv/0110_temp.csv')
 
 }
 
