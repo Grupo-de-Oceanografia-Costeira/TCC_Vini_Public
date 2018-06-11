e17c820fd8f170d86a12ee47a0f79ac8dc287916
Vini Salazar <viniws@gmail.com>
2018-06-11 13:34:10 -0300

    working and renaming plots

---
+++ analysis/plots/section_temperature.py
@@ -0,0 +1,107 @@
+'''
+'''
+# Importing libraries
+import pandas as pd
+from code.functions import *
+
+saida1 = 'data/ctd/stations_25-01-2017_processed.cnv'
+saida2 = 'data/ctd/stations_27-05-2017_processed.cnv'
+saida3 = 'data/ctd/stations_08-07-2017_processed.cnv'
+saida4 = 'data/ctd/stations_01-10-2017_processed.cnv'
+
+# Loading the data
+hd1, hd2, variables, datapoints, alldata = load(saida1)
+
+# Loading metadata
+df = pd.read_csv('data/csv/coordenadas.csv', sep = ';')
+dates = set(df['Data'])
+dates = list(dates)
+today = dates[2]
+stations = list(df.loc[df['Data'] == today]['Ponto'])
+lat = list(df.loc[df['Data'] == today]['Lat'])
+lon = list(df.loc[df['Data'] == today]['Lon'])
+
+
+[i.insert(3,'test') for i in [stations, lat, lon]] # saida1 e saida4
+# stations, lat, lon = ['test'] + stations, ['test'] + lat, ['test'] + lon # saida2
+
+# Splitting data into different stations
+d = split_stations(datapoints, stations, variables, lat, lon)
+
+# Removing upcasts
+for st in d:
+    d[st] = remove_upcast(d[st])
+
+# Creating variables with stations from the dictionary
+locals().update(d)
+
+# Plot dependencies
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import geopandas as gp
+import numpy as np
+
+def sectionplot(arg, arg2 = None, arg3 = None):
+    # Arrays storing salinity data in sals list
+    sals = []
+    for i in arg:
+        sals.append(np.array(i['t090:']))
+
+    # Setting salinity range for colorbar
+    sal_range = np.arange(1, 40.1, 1)
+
+    # Arrays storing depth data in deps list
+    deps = []
+    for i in arg:
+        deps.append(np.array(i['depSM:']))
+
+    # Setting the x axis values for salinity
+    x = np.array([])
+    ix = 0
+    for i in sals:
+        x = np.append(x, [ix]*len(i))
+        ix += 1
+
+    # Setting the y axis values for depth
+    y = np.array([])
+    ix = 0
+    for i in deps:
+        y = np.append(y, i)
+        ix += 1
+
+    # Setting the color values
+    z = np.concatenate(tuple(sals))
+
+    # Generating the gridded data
+    xi = np.linspace(np.min(x), np.max(x), 200)
+    yi = np.linspace(np.min(y), np.max(y), 200)
+    xi, yi = np.meshgrid(xi, yi)
+    zi = mlab.griddata(x, y, z, xi, yi, interp='linear') #interp = 'linear' se der erro no Natgrid
+
+    # Plotting the gridded data
+    plt.figure() # Starting the figure object
+    plt.pcolormesh(xi,yi,zi, cmap='jet') # Adding the colour mesh
+    plt.contour(xi, yi, zi, colors='k') # Contour lines
+    plt.scatter(x,y,c=z, cmap='jet') # Adding the scatter points
+    plt.xticks(range(0, len(arg)+1), ["Estacao " + i['STATION'][0][2:] for i in arg])
+    plt.colorbar().set_label('Temperatura em C')
+    plt.axis([np.min(x), np.max(x), np.min(y), np.max(y)])
+    plt.gca().invert_yaxis()
+    plt.ylabel('Profundidade (m)')
+
+    if arg2:
+        plt.title(arg2 + ' - ' + today)
+
+    if arg3:
+        plt.savefig(arg3+arg2.split()[0].strip()+'_section_'+today, transparent=True)
+    else:
+        plt.show()
+
+
+if __name__ == '__main__':
+    arg = [st1, st7, st11, st14] # Test this with other stations
+    sectionplot(arg, 'Canal da margem leste', './img/section/temperature/')
+
+if __name__ == '__main__':
+    arg = [st4, st5, st6] # Test this with other stations
+    sectionplot(arg, 'Rio Tubarao', './img/section/temperature/')
---
+++ analysis/plots/surface_sal.py
@@ -0,0 +1,82 @@
+'''
+Area salinity plot, can be either surface or bottom temperature.
+'''
+
+# Importando as bibliotecas
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.io.shapereader import Reader
+import csv
+import geopandas as gp
+import pandas as pd
+import numpy as np
+
+def main(arg, arg2=None):
+
+	# Shapefiles que vão gerar os limites territoriais.
+	sc = 'analysis/cartopy/shapefiles/SC-POLYGON.shp'
+	laguna = 'analysis/cartopy/shapefiles/DestaqueLaguna-POLYGON.shp'
+	lagoas = 'analysis/cartopy/shapefiles/LagoasComplexoLagunar-POLYGON.shp'
+	rios = gp.read_file('analysis/cartopy/shapefiles/RiosComplexoLagunar.shp')
+
+	# Vamos fazer uma lista para iterar um loop com a função add_geometries()
+	shapes = [sc, laguna, lagoas, rios]
+
+	for key in arg:
+
+		fig = plt.figure()
+
+		# Iniciando a projeção e suas dimensões lat/lon
+		ax = plt.axes(projection=ccrs.PlateCarree())
+		coords = (-48.9, -48.7, -28.4, -28.55)
+		ax.set_extent(coords)
+		ax.gridlines(draw_labels=True)
+
+		# Os pontos de coleta e os dados vêm de arquivos csv
+		lats = arg[key][0].values
+		lons = arg[key][1].values
+		surf_data = arg[key][2].values
+		bot_data = arg[key][3].values
+		dmy = arg[key][4]
+
+		# Imagem de background
+		ax.stock_img()
+
+		# Adicionando os shapes ao plot
+		ax.add_geometries(Reader(sc).geometries(),
+			ccrs.PlateCarree(),
+			facecolor = 'beige', edgecolor = 'black')
+
+		ax.add_geometries(Reader(lagoas).geometries(),
+			ccrs.PlateCarree(),
+			facecolor = 'skyblue')
+
+		ax.text(-48.745, -28.41, dmy, zorder=9,
+		 bbox = dict(facecolor='white', alpha=0.5))
+
+		rios.plot(ax=ax, color = 'skyblue', lw=4.0)
+
+		plt.scatter(lons, lats+0.003, c=surf_data, zorder=11, s=50, vmin=18, vmax=32, cmap = 'viridis')
+		plt.scatter(lons, lats, c=bot_data, zorder=10, s=50, vmin=18, vmax=32, cmap = 'viridis')
+		plt.colorbar(shrink=0.7, ticks=range(18,31,4), pad=0.125,
+		).set_label('Salinidade')
+
+		if arg2:
+			plt.savefig(arg2 + dmy + '_salinity', transparent=True)
+		else:
+			plt.show()
+
+df = pd.read_csv('data/csv/coordenadas.csv', delimiter = ';')
+
+all = {
+
+'saida1' : (df.loc[df['Data'] == '25-Jan-17']['Lat'], df.loc[df['Data'] == '25-Jan-17']['Lon'], df.loc[df['Data'] == '25-Jan-17']['Surf Sal'], df.loc[df['Data'] == '25-Jan-17']['Bot Sal'], '25-Jan-2017'),
+'saida2' : (df.loc[df['Data'] == '27-May-17']['Lat'], df.loc[df['Data'] == '27-May-17']['Lon'], df.loc[df['Data'] == '27-May-17']['Surf Sal'], df.loc[df['Data'] == '27-May-17']['Bot Sal'], '27-May-2017'),
+'saida3' : (df.loc[df['Data'] == '08-Jul-17']['Lat'], df.loc[df['Data'] == '08-Jul-17']['Lon'], df.loc[df['Data'] == '08-Jul-17']['Surf Sal'], df.loc[df['Data'] == '08-Jul-17']['Bot Sal'], '08-Jul-2017'),
+'saida4' : (df.loc[df['Data'] == '01-Oct-17']['Lat'], df.loc[df['Data'] == '01-Oct-17']['Lon'], df.loc[df['Data'] == '01-Oct-17']['Surf Sal'], df.loc[df['Data'] == '01-Oct-17']['Bot Sal'], '01-Oct-2017')
+
+}
+
+if __name__ == '__main__':
+	main(all, 'img/salinity/both')
