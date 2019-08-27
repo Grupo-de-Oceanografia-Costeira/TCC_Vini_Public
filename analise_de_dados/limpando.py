a6769285f773dfcb9a0d64f0c64fb9ce912478ce
Vini Salazar <viniws@gmail.com>
2019-08-27 02:42:17 -0300

    Developing data script

--- analise_de_dados/limpando.py
+++ analise_de_dados/limpando.py
@@ -1,6 +1,8 @@
+import numpy as np
 import seaborn as sns
 from os import path
 from collections import OrderedDict
+from matplotlib import pyplot as plt
 
 
 class Saida:
@@ -66,12 +68,12 @@ class Saida:
         ix_ = 0
         values = pd.DataFrame()
         for ix, row in self.df.iterrows():
-            if ix_ < len(verao.coordenadas):
+            if ix_ < len(self.coordenadas):
                 if row["c0S/m"] >= 0.1:
                     station, lat, lon = (
-                        verao.coordenadas.Ponto.iloc[ix_],
-                        verao.coordenadas.Lat.iloc[ix_],
-                        verao.coordenadas.Lon.iloc[ix_],
+                        self.coordenadas.Ponto.iloc[ix_],
+                        self.coordenadas.Lat.iloc[ix_],
+                        self.coordenadas.Lon.iloc[ix_],
                     )
                     row["name"] = station
                     row["lat"] = lat
@@ -81,20 +83,72 @@ class Saida:
                     if len(values) < 4:
                         values = pd.DataFrame()
                     elif len(values) >= 4:
-                        print(f"Load station {verao.coordenadas.Ponto.iloc[ix_]}.")
-                        self.stations[verao.coordenadas.Ponto.iloc[ix_]] = values
+                        print(f"Load station {self.coordenadas.Ponto.iloc[ix_]}.")
+                        self.stations[self.coordenadas.Ponto.iloc[ix_]] = values
                         ix_ += 1
                         values = pd.DataFrame()
 
 
+# Carregando dados
 verao = Saida("data/ctd/stations_25-01-2017_processed.cnv")
-coordenadas = pd.read_csv("data/csv/coordenadas.csv", sep=";")
-verao.coordenadas = coordenadas[coordenadas.Data.str.contains("25")]
-verao.split_stations()
-verao.get_upcast()
-verao.upcast
+outono = Saida("data/ctd/stations_27-05-2017_processed.cnv")
+inverno = Saida("data/ctd/stations_08-07-2017_processed.cnv")
+primavera = Saida("data/ctd/stations_01-10-2017_processed.cnv")
+cc = pd.read_csv("data/csv/coordenadas_mare.csv", sep="\t")
 
-# sns.distplot(verao.upcast["c0S/m"])
-# sns.distplot(verao.upcast["t090"])
-# sns.distplot(verao.upcast["pr"])
-# sns.distplot(verao.upcast["sal00"])
+# Carregando coordenadas
+verao.coordenadas = cc[cc.Data.str.contains("25-Jan")]
+outono.coordenadas = cc[cc.Data.str.contains("27-May")]
+inverno.coordenadas = cc[cc.Data.str.contains("08-Jul")]
+primavera.coordenadas = cc[cc.Data.str.contains("01-Oct")]
+
+# Separando estações
+for season in (verao, outono, inverno, primavera):
+    season.get_upcast()
+    season.split_stations()
+
+
+# Limpando dados de clorofila
+cc["CC"] = cc.CC.apply(lambda x: np.nan if x < 0 else x)
+cc = cc[cc["CC"].notnull()]
+cc["Estrat Temp"] = abs(cc["Surf Temp"] - cc["Bot Temp"])
+cc["Estrat Sal"] = abs(cc["Surf Sal"] - cc["Bot Sal"])
+cc["Secchi"] = pd.to_numeric(cc["Secchi"])
+
+sns.lmplot("Surf Temp", "CC", data=cc)
+sns.lmplot("Bot Temp", "CC", data=cc)
+sns.lmplot("Estrat Temp", "CC", data=cc)
+sns.lmplot("Surf Sal", "CC", data=cc)
+sns.lmplot("Bot Sal", "CC", data=cc)
+sns.lmplot("Estrat Sal", "CC", data=cc)
+sns.distplot(cc["Estrat Sal"])
+
+df = []
+for season, season_name in zip(
+    [verao, outono, inverno, primavera], ["verao", "outono", "inverno", "primavera"]
+):
+    for station, df_ in season.stations.items():
+        df_["season"] = season_name
+        df.append(df_)
+
+df = pd.concat(df)
+
+
+def get_zona(st):
+    zonas = {
+        "Canal": ("st1", "st2", "st3", "st7", "st11", "st14"),
+        "Rio Tubarão": ("st4", "st5", "st6"),
+        "Meio": ("st8", "st9", "st10", "st12", "st13", "st15", "st16", "st17"),
+        "Ponte": ("st18", "st19", "st20"),
+    }
+    for k, v in zonas.items():
+        if st in v:
+            return k
+
+
+df["zona"] = df.name.apply(lambda s: get_zona(s))
+
+df.to_csv("data/csv/todas.csv", sep="\t", index=False)
+
+
+# cc.to_csv("data/csv/coordenadas_processadas.csv", sep="\t", index=False)
